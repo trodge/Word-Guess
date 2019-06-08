19ee34ea515a0e5863c996dbafbd8e8132fb318d
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-06-08 15:54:57 -0700

    made the layout totally sick

--- scripts.js
+++ scripts.js
@@ -15,24 +15,24 @@ class QuestionAnswer {
             if (this.answer[i] === " ") { 
                 underscores += "&nbsp; ";
                 ++this.guessed;
-            } else if (".,\"'".indexOf(this.answer[i]) !== -1) {
+            } else if ("!.,\"'".indexOf(this.answer[i]) !== -1) {
                 underscores += this.answer[i] + " ";
                 ++this.guessed;
             } else underscores += "_ ";
         }
         document.getElementById("answer").innerHTML = underscores;
-        document.getElementById("wrong").innerHTML = "Wrong :";
-        document.getElementById("correct").innerHTML = "";
+        document.getElementById("wrong").innerHTML = "Wrong: ";
     }
 
     tryLetter(letter) {
         // Check if letter is in answer and if so fill in those letters
         // Return if letter is one of the characters we auto-fill
-        if (" .,\"'".indexOf(letter) !== -1) return;
+        if (" !.,\"'".indexOf(letter) !== -1) return;
         var lowerAnswer = this.answer.toLowerCase();
         var i = lowerAnswer.indexOf(letter);
         if (i == -1 && this.wrong.indexOf(letter) == -1) {
             this.wrong += letter;
+            ++wrong;
             document.getElementById("wrong").innerHTML = "Wrong: " + this.wrong;
         }
         for (; i > -1; i = lowerAnswer.indexOf(letter, i + 1)) {
@@ -43,10 +43,10 @@ class QuestionAnswer {
             if (oldAnswer[i * 2] == "_") {
                 document.getElementById("answer").innerHTML = oldAnswer.substring(0, i * 2) + this.answer[i] + oldAnswer.substring(i * 2 + 1);
                 ++this.guessed;
-                if (this.guessed == this.answer.length) document.getElementById("correct").innerHTML = "Correct! Press any key to continue";
+                if (this.guessed == this.answer.length) document.getElementById("wrong").innerHTML = "Correct! Press any key to continue";
             }
             // Put non-breaking spaces back
-            document.getElementById("answer").innerHTML = document.getElementById("answer").innerHTML.replace(/  /g, "&nbsp; ");
+            document.getElementById("answer").innerHTML = document.getElementById("answer").innerHTML.replace(/   /g, " &nbsp; ");
         }
     }
 };
@@ -55,10 +55,17 @@ var QAs = [new QuestionAnswer("O ____, ____! wherefore art thou _____?", "Romeo"
 new QuestionAnswer("Hamlet's mother", "Gertrude"),
 new QuestionAnswer("Two households, both alike in dignity,<br>In fair Verona, where we lay our scene.<br>From ancient grudge break to new ______,", "mutiny"),
 new QuestionAnswer("Romeo's last words", "Thus with a kiss I die."),
-new QuestionAnswer("Prospero's daughter", "Miranda")]
+new QuestionAnswer("Prospero's daughter", "Miranda"),
+new QuestionAnswer("Prince of Norway", "Fortinbras"),
+new QuestionAnswer("Setting of Twelfth Night","Illyria"),
+new QuestionAnswer("The play Hamlet stages for Claudius", "The Murder of Gonzago"),
+new QuestionAnswer("What does Hamlet say while holding a skull?", "Alas, poor Yorick! I knew him, Horatio")]
+// Pick a random question to start with.
 var i = Math.floor(Math.random() * QAs.length);
 console.log(i);
 QAs[i].ask();
+// Track the total wrong guesses
+var wrong = 0;
 document.addEventListener("keypress", function (event) {
     if (QAs[i].guessed === QAs[i].answer.length) {
         // Question i has been answered
@@ -67,8 +74,7 @@ document.addEventListener("keypress", function (event) {
             // There are no more questions to ask
             document.getElementById("question").innerHTML = "You win!";
             document.getElementById("answer").innerHTML = "";
-            document.getElementById("wrong").innerHTML = "";
-            document.getElementById("correct").innerHTML = "";
+            document.getElementById("wrong").innerHTML = "Total wrong: " + wrong;
         } else {
             i = Math.floor(Math.random() * QAs.length);
             QAs[i].ask();
