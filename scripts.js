441ac1eca2f15dc729eee6b9f3d5fadc727dafac
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-06-08 10:37:47 -0700

    added more questions

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ class QuestionAnswer {
         this.question = question;
         this.answer = answer;
         this.guessed = 0;
-        this.wrong = 0;
+        this.wrong = "";
     }
 
     ask() {
@@ -15,19 +15,25 @@ class QuestionAnswer {
             undescores += "_ ";
         }
         document.getElementById("answer").innerHTML = undescores;
+        document.getElementById("wrong").innerHTML = "Wrong :";
+        document.getElementById("correct").innerHTML = "";
     }
 
     tryLetter(letter) {
         // Check if letter is in answer and if so fill in those letters
         var lowerAnswer = this.answer.toLowerCase();
         var i = lowerAnswer.indexOf(letter);
-        if (i == -1) ++this.wrong;
+        if (i == -1 && this.wrong.indexOf(letter) == -1) {
+            this.wrong += letter;
+            document.getElementById("wrong").innerHTML = "Wrong: " + this.wrong;
+        }
         for (; i > -1; i = lowerAnswer.indexOf(letter, i + 1)) {
             // Found the letter at index i
             var oldAnswer = document.getElementById("answer").innerHTML;
             if (oldAnswer[i * 2] == "_") {
                 document.getElementById("answer").innerHTML = oldAnswer.substring(0, i * 2) + this.answer[i] + oldAnswer.substring(i * 2 + 1);
                 ++this.guessed;
+                if (this.guessed == this.answer.length) document.getElementById("correct").innerHTML = "Correct! Press any key to continue";
             }
         }
     }
@@ -40,16 +46,18 @@ var i = Math.floor(Math.random() * QAs.length);
 console.log(i);
 QAs[i].ask();
 document.addEventListener("keypress", function (event) {
-    QAs[i].tryLetter(event.key);
     if (QAs[i].guessed === QAs[i].answer.length) {
         // Question i has been answered
         QAs.splice(i, 1);
         if (QAs.length === 0) {
+            // There are no more questions to ask
             document.getElementById("question").innerHTML = "You win!";
             document.getElementById("answer").innerHTML = "";
+            document.getElementById("wrong").innerHTML = "";
+            document.getElementById("correct").innerHTML = "";
         } else {
             i = Math.floor(Math.random() * QAs.length);
             QAs[i].ask();
         }
-    }
+    } else QAs[i].tryLetter(event.key);
 });
